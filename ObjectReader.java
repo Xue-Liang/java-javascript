0e7d41b992c164f5a5284cbc9633d3f4571ccf12
XueLiang <xue2005@qq.com>
2017-02-13 14:30:14 +0800

    Update ObjectReader.java

--- ObjectReader.java
+++ ObjectReader.java
@@ -9,24 +9,18 @@ import java.io.ObjectInputStream;
  * Created by xue on 2017-01-19.
  */
 public class ObjectReader<T> {
-
-    public <T> T readObjectFrom(File f) {
-        if (!f.exists()) {
-            throw new IllegalArgumentException("文件" + f.getName() + "不存在,无法读取.");
-        }
-        InputStream is = new FileInputStream(f);
-        try (ObjectInputStream ois = new ObjectInputStream(is)) {
-            return (T) ois.readObject();
+    public static <T> T readObjectFrom(File f) {
+        try (InputStream is = new FileInputStream(f)) {
+            return readObjectFrom(is);
         } catch (Exception e) {
             return null;
         }
     }
 
-    public <T> T readObjectFrom(InputStream is) {
+    public static <T> T readObjectFrom(InputStream is) {
         if (is == null) {
-            throw new IllegalArgumentException("输入流为空,无法读取.");
+            return null;
         }
-
         try (ObjectInputStream ois = new ObjectInputStream(is)) {
             return (T) ois.readObject();
         } catch (Exception e) {
